b6085af25c2d404cdae83543c1eef81013193d38
GusVilla303 <gusvilla303@gmail.com>
2014-11-20 11:20:21 -0700

    Create github scopes (attributes) to user email and ability to read user's organizations

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,read:org"
 end
