9d93e8aac6fc3eed8036f3f1817a583ff2474a13
GusVilla303 <gusvilla303@gmail.com>
2014-11-20 14:57:31 -0700

    Integration with Github is works, SignIn and SignOut work

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :user
-    get '/login' => redirect('/auth/github'), as: :login
-    get '/auth/github/callback', to:'sessions#create'
-    get '/signout' => 'sessions#destroy', as: :signout
+  get '/login'                => redirect('/auth/github'), as: :login
+  get '/signout'              => 'sessions#destroy',       as: :signout
+  get '/auth/github/callback' => 'sessions#create'
 end
