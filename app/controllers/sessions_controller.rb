dea0d34743472a0035d46c8eb2fc82c8768347de
Horacio Chavez <horacio.hch@gmail.com>
2014-11-23 15:41:03 -0700

    Add a test to user can login successfully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    @nickname = auth[:info][:nickname]
+    nickname = auth[:info][:nickname]
     if belongs_to_turing?(nickname)
       user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Signed in!"
+      redirect_to root_url, notice: "Signed in!"
     else
-      redirect_to root_url
+      redirect_to root_url, notice: "You're not that cool!"
     end
   end
 
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   private
 
   def belongs_to_turing?(nickname)
-    url         = "https://api.github.com/users/#{@nickname}/orgs"
+    url         = "https://api.github.com/users/#{nickname}/orgs"
     response    = Faraday.get(url)
     orgs        = JSON.parse(response.body)
     turing_hash = orgs.find { |org| org['id'] == 7934292 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  resources :jobs, except:[ :create, :destroy]
+  resources :jobs, except:[:create, :destroy]
 
   get '/login'                => redirect('/auth/github'), as: :login
   get '/signout'              => 'sessions#destroy',       as: :signout
