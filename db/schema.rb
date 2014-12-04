90a24b4b91d37043ef3039139e8c2710675a8100
Horacio Chavez <horacio.hch@gmail.com>
2014-12-04 13:40:48 -0700

    all but home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203210332) do
+ActiveRecord::Schema.define(version: 20141204195528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20141203210332) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nickname"
+    t.string   "image"
   end
 
 end
