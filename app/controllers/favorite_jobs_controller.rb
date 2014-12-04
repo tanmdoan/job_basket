312b494208b650bab5c55910f6c721f17b7bbd29
Horacio Chavez <horacio.hch@gmail.com>
2014-12-03 22:33:26 -0700

    added favorites partial

--- app/controllers/favorite_jobs_controller.rb
+++ app/controllers/favorite_jobs_controller.rb
@@ -3,27 +3,23 @@ class FavoriteJobsController < ApplicationController
   def index
     @favorite_jobs = current_user.favorite_jobs
   end
-  def create
 
+  def create
     @favorite_job = current_user.favorite_jobs.create(job_id: params[:job_id])
 
     respond_to do |format|
       if @favorite_job.save
         format.html { redirect_to @favorite_job, notice: 'Favorited job.' }
-        format.js   {flash[:notice] = 'Job Favorited'}
-        format.json { render json: @favorite_job, status: :created, location: @favorite_job, notice: 'Job Favorited' }
-
+        format.js   { flash[:notice] = 'Job Favorited' }
+        format.json { render  json: @favorite_job,
+                              status: :created,
+                              location: @favorite_job,
+                              notice: 'Job Favorited' }
       else
-        # format.html { render action: "new" }
-        format.js {flash[:error] = "You already favorited this job!"}
-        format.json { render json: @favorite_job.errors, status: :unprocessable_entity }
+        format.js   {flash[:error] = "You already favorited this job!"}
+        format.json { render  json: @favorite_job.errors,
+                              status: :unprocessable_entity }
       end
     end
   end
-
-
-
-  private
-
-
 end
