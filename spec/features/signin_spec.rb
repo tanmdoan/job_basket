221b537c0887cc39b10ba878d9f74a47e8cc89d6
Jeff Casimir <jeff@casimircreative.com>
2014-12-10 08:07:58 -0700

    Pew\! Pew\! Spec failures going down\!

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "Login with GitHub", type: :feature do
     it "has authentication errors" do
       OmniAuth.config.mock_auth[:github] = :invalid_credentials
       visit root_path
-      click_link "Connect with Github"
+      click_link_or_button 'login'
       expect(page).to have_content("Sorry, login failed!")
     end
   end
