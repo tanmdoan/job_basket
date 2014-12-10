b21bc9d0d8a8f4cfa00b4f7141baed2701ace450
Jeff Casimir <jeff@casimircreative.com>
2014-12-10 08:59:37 -0700

    Simplifying OmniAuth mocking

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe "Login with GitHub", type: :feature do
 
   describe "user with valid information" do
     it "is successful" do
-
-      visit root_path
-      expect(page).to have_css('#login')
-
       login
-      expect(page).to have_content "Jobs"
     end
   end
 
--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -1,27 +1,23 @@
-def github_mock_auth
-  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
-    provider: 'github',
-    uid: '123',
-    info: {
-      name: 'Horacio Chavez',
-      email: 'horacio@example.com',
-      image: 'horacio.png',
-      nickname: 'HoracioChavez'
-    },
-    credentials: {
-      token: 'mock_token'
-    },
-    extra:{
-      raw_info: {
-        avatar_url: 'http://gravatar.com/horacio.jpg'
-      }
+OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+  provider: 'github',
+  uid: '123',
+  info: {
+    name: 'Horacio Chavez',
+    email: 'horacio@example.com',
+    image: 'horacio.png',
+    nickname: 'HoracioChavez'
+  },
+  credentials: {
+    token: 'mock_token'
+  },
+  extra:{
+    raw_info: {
+      avatar_url: 'http://gravatar.com/horacio.jpg'
     }
-  })
-end
-
+  }
+})
 
 def login
   visit root_path
-  github_mock_auth
   click_link_or_button('login')
 end
